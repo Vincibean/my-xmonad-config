3138f4ba44212ef8c17fa05cd6343bba6dd4846d
Andrea Bessi <andrebessi00@gmail.com>
2018-01-05 01:51:35 +0100

    Further simplified

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,47 +1,11 @@
-import XMonad hiding (Tall)
-import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.ManageDocks
-import XMonad.Hooks.UrgencyHook
-import XMonad.Layout.LayoutHints
-import XMonad.Layout.ResizableTile
-import XMonad.Prompt
-import XMonad.Prompt.Shell
-import XMonad.Util.Run(spawnPipe)
+import XMonad
+import XMonad.Config.Desktop
 
 import System.Exit
 import System.IO
-import Data.Monoid
 
-import qualified XMonad.StackSet as W
-import qualified Data.Map        as M
-
-------------------------------------------------------------------------
--- Workspaces
--- The default number of workspaces (virtual screens) and their names.
---
-myWorkspaces = map show [1..9]
-
-------------------------------------------------------------------------
--- Run xmonad with all the defaults we set up.
---
 main = do
     xmproc <- spawnPipe "xmobar"
-    xmonad $ withUrgencyHook NoUrgencyHook
-           $ def {
-                terminal           = "urxvt",
-                workspaces         = myWorkspaces,
-                normalBorderColor  = "#333333",
-                focusedBorderColor = "#3399cc",
-                layoutHook         = myLayout,
-                logHook            = dynamicLogWithPP $ xmobarPP { 
-                                         ppOutput = hPutStrLn xmproc,
-                                         ppTitle = xmobarColor "green" "" . shorten 50
-                                     }
-    }
-
-myLayout = avoidStruts $ layoutHints (tall ||| Mirror tall ||| Full)
-  where
-     tall = ResizableTall nmaster delta ratio []
-     nmaster = 1
-     delta   = 3/100
-     ratio   = 1/2
+    xmonad desktopConfig
+        { terminal    = "urxvt"
+        }
