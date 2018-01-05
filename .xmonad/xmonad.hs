4b018a5119fa182e82ee109a872fbd8433946f5d
Andrea Bessi <andrebessi00@gmail.com>
2018-01-05 01:39:05 +0100

    Removed manageHooks

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -22,23 +22,6 @@ import qualified Data.Map        as M
 myWorkspaces = map show [1..9]
 
 ------------------------------------------------------------------------
--- Window rules
--- Execute arbitrary actions and WindowSet manipulations when managing
--- a new window. You can use this to, for example, always float a
--- particular program, or have a client always appear on a particular
--- workspace.
---
--- To find the property name associated with a program, use
--- > xprop | grep WM_CLASS
--- and click on the client you're interested in.
---
--- To match on the WM_NAME, you can use 'title' in the same way that
--- 'className' and 'resource' are used below.
---
-myManageHook :: ManageHook
-myManageHook = manageDocks <+> manageHook defaultConfig
-
-------------------------------------------------------------------------
 -- Run xmonad with all the defaults we set up.
 --
 main = do
@@ -49,7 +32,6 @@ main = do
                 workspaces         = myWorkspaces,
                 normalBorderColor  = "#333333",
                 focusedBorderColor = "#3399cc",
-                manageHook         = myManageHook,
                 layoutHook         = myLayout,
                 logHook            = dynamicLogWithPP $ xmobarPP { 
                                          ppOutput = hPutStrLn xmproc,
