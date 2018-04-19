21890c07d4f86f0b4e28732ae161e76fb0f1be05
Vincibean <andrebessi00@gmail.com>
2018-04-19 22:04:42 +0000

    updated xmonad.hs and .xmobarrc

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,12 +1,21 @@
 import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
 import XMonad.Config.Desktop
 import XMonad.Util.Run(spawnPipe)
 
 import System.Exit
 import System.IO
 
+myWorkspaces = map show [0..9]
+
+myLogHook = dynamicLogWithPP $ xmobarPP
+
 main = do
     xmproc <- spawnPipe "xmobar"
     xmonad desktopConfig
-        { terminal    = "urxvt"
+        { terminal    = "urxvt",
+          layoutHook  = avoidStruts $ layoutHook def,
+          logHook     = myLogHook,
+          manageHook  = manageDocks <+> manageHook def
         }
