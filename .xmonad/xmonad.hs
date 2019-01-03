6d4368e04dada1c2db3cc7c3636aff01caa7f570
Vincibean <andrebessi00@gmail.com>
2019-01-03 14:26:55 +0000

    added screen locking with slock

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -3,15 +3,18 @@ import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Config.Desktop
 import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
 
 import System.Exit
 import System.IO
 
 main = do
     xmproc <- spawnPipe "xmobar"
-    xmonad desktopConfig
+    xmonad $ desktopConfig
         { terminal   = "urxvt",
           layoutHook = avoidStruts  $  layoutHook def,
           logHook    = dynamicLogWithPP $ xmobarPP,
           manageHook = manageDocks <+> manageHook def
-        }
+        } `additionalKeys`
+        [ ((mod1Mask .|. shiftMask, xK_z), spawn "slock")
+        ]
