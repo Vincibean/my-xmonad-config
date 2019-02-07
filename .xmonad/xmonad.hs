707b734ce0e32ad3ab32c0a4c7f0e9d67c3d1872
Vincibean <andrebessi00@gmail.com>
2019-02-07 14:07:17 +0000

    fixed blank screen issue when opening Java Swing applications

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,6 +1,7 @@
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.SetWMName
 import XMonad.Config.Desktop
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
@@ -11,10 +12,11 @@ import System.IO
 main = do
     xmproc <- spawnPipe "xmobar"
     xmonad $ desktopConfig
-        { terminal   = "urxvt",
-          layoutHook = avoidStruts  $  layoutHook def,
-          logHook    = dynamicLogWithPP $ xmobarPP,
-          manageHook = manageDocks <+> manageHook def
+        { terminal    = "urxvt",
+          layoutHook  = avoidStruts  $  layoutHook def,
+          logHook     = dynamicLogWithPP $ xmobarPP,
+          startupHook = setWMName "LG3D",
+          manageHook  = manageDocks <+> manageHook def
         } `additionalKeys`
         [ ((mod1Mask .|. shiftMask, xK_z), spawn "slock")
         ]
